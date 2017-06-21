f352b616fd6443ed17ee8178ea1af4dbeb2b44cd
Lin <lin591025@163.com>
2017-06-21 17:01:10 +0800

    删除不必要的东西

--- zhuanghaojia/mydecorate/src/main/java/com/tobosu/mydecorate/view/OnRangeBarChangeListener.java
+++
@@ -1,8 +0,0 @@
-package com.tobosu.mydecorate.view;
-
-/**
- * Created by tangyx on 16/8/25.
- */
-public interface OnRangeBarChangeListener {
-    void onIndexChangeListener(BaseSeekBar rangeBar, int leftThumbIndex, int rightThumbIndex);
-}
