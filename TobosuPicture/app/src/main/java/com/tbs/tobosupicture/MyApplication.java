bdd71f97e3ca151698fbcfa3e9e49eb5d16e658f
Lin <lin591025@163.com>
2017-06-27 15:26:44 +0800

    新建图库App项目

---
+++ TobosuPicture/app/src/main/java/com/tbs/tobosupicture/MyApplication.java
@@ -0,0 +1,14 @@
+package com.tbs.tobosupicture;
+
+import android.app.Application;
+
+/**
+ * Created by Mr.Lin on 2017/5/10 15:40.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ TobosuPicture/app/src/main/java/com/tbs/tobosupicture/activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.tbs.tobosupicture.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Mr.Lin on 2017/5/10 15:41.
+ */
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
