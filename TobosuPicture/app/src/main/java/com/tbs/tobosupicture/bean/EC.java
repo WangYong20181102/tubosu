3fcc6b8108d5b53bbd5a3cf22dabf16498fb22cb
Lin <lin591025@163.com>
2017-09-07 10:52:32 +0800

    我的 页面中的搜索过的案例红点提示

--- TobosuPicture/app/src/main/java/com/tbs/tobosupicture/bean/EC.java
+++ TobosuPicture/app/src/main/java/com/tbs/tobosupicture/bean/EC.java
@@ -36,6 +36,8 @@ public final class EC {
         public static final int MY_ORGIN_MSG = 0x000025;//拿到我发起消息
         public static final int MY_JOIN_MSG = 0x000026;//拿到我参与的消息
         public static final int REFRESH_MY_ORGIN = 0x000027;//刷新我的发起数据（用户第一次发动态的时候调用）
+        public static final int SHOW_MINE_RED_DOT = 0x000028;//显示我的界面中的搜索装修案例的红点提示
+        public static final int HINT_MINE_RED_DOT = 0x000029;//隐藏我的界面中的搜索装修案例的红点提示
 
 
 //        public static final int CHOOSE_CITY_CODE = 0x100001;
