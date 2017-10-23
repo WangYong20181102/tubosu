0c66b9ae92ebadebb6cb49ce0b3f203b764c929d
Lin <lin591025@163.com>
2017-10-23 09:12:13 +0800

    修改显示回复空格问题

--- TobosuPicture/app/src/main/java/com/tbs/tobosupicture/activity/MainActivity.java
+++ TobosuPicture/app/src/main/java/com/tbs/tobosupicture/activity/MainActivity.java
@@ -232,54 +232,59 @@ public class MainActivity extends BaseActivity {
         public void call(Object... args) {
             Log.e(TAG, "接收到服务器的推送消息 ===没有处理前===" + args[0].toString());
             //base64解密
-            String json = Base64Util.getFromBase64(args[0].toString());
-            if (!TextUtils.isEmpty(json)) {
-                Log.e(TAG, "解密之后的数据=======" + json);
-                try {
-                    JSONObject jsonObject = new JSONObject(json);
-                    String type = jsonObject.getString("type");
-                    if (type.equals("1")) {
-                        //动态消息
-                        receiveMsg = mGson.fromJson(json, _ReceiveMsg.class);
-                        //我的发起的消息 包含消息的数量消息的头像
-                        EventBusUtil.sendEvent(new Event(EC.EventCode.MY_JOIN_MSG, receiveMsg.getMy_participation()));//我的参与
-                        EventBusUtil.sendEvent(new Event(EC.EventCode.MY_ORGIN_MSG, receiveMsg.getMy_sponsor()));//我的发起
-                        //将数值布局
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                //显示数量
-                                int msgNum = Integer.parseInt(receiveMsg.getAll_msg_count());
-                                if (msgNum >= 100) {
-                                    mImgToFriendTag.setText("99+");
-                                } else {
-                                    mImgToFriendTag.setBadgeCount(msgNum);
-                                }
+            if (args[0].toString().equals("Array")) {
+                Log.e(TAG, "出现了Array bug =================");
+            } else {
+                String json = Base64Util.getFromBase64(args[0].toString());
+                if (!TextUtils.isEmpty(json)) {
+                    Log.e(TAG, "解密之后的数据=======" + json);
+                    try {
+                        JSONObject jsonObject = new JSONObject(json);
+                        String type = jsonObject.getString("type");
+                        if (type.equals("1")) {
+                            //动态消息
+                            receiveMsg = mGson.fromJson(json, _ReceiveMsg.class);
+                            //我的发起的消息 包含消息的数量消息的头像
+                            EventBusUtil.sendEvent(new Event(EC.EventCode.MY_JOIN_MSG, receiveMsg.getMy_participation()));//我的参与
+                            EventBusUtil.sendEvent(new Event(EC.EventCode.MY_ORGIN_MSG, receiveMsg.getMy_sponsor()));//我的发起
+                            //将数值布局
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //显示数量
+                                    int msgNum = Integer.parseInt(receiveMsg.getAll_msg_count());
+                                    if (msgNum >= 100) {
+                                        mImgToFriendTag.setText("99+");
+                                    } else {
+                                        mImgToFriendTag.setBadgeCount(msgNum);
+                                    }
 
-                            }
-                        });
-                    } else if (type.equals("2")) {
-                        //新案例消息
-                        is_exist_case = jsonObject.getString("is_exist_case");
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                if (is_exist_case.equals("1")) {
-                                    mianAboutReddot.setVisibility(View.VISIBLE);
-                                    EventBusUtil.sendEvent(new Event(EC.EventCode.SHOW_MINE_RED_DOT));//在我的界面中显示红点
-                                } else {
-                                    Log.e(TAG, "隐藏‘我的’红点提示======");
-                                    mianAboutReddot.setVisibility(View.GONE);
-                                    EventBusUtil.sendEvent(new Event(EC.EventCode.HINT_MINE_RED_DOT));//在我的界面中隐藏红点
                                 }
-                            }
-                        });
+                            });
+                        } else if (type.equals("2")) {
+                            //新案例消息
+                            is_exist_case = jsonObject.getString("is_exist_case");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (is_exist_case.equals("1")) {
+                                        mianAboutReddot.setVisibility(View.VISIBLE);
+                                        EventBusUtil.sendEvent(new Event(EC.EventCode.SHOW_MINE_RED_DOT));//在我的界面中显示红点
+                                    } else {
+                                        Log.e(TAG, "隐藏‘我的’红点提示======");
+                                        mianAboutReddot.setVisibility(View.GONE);
+                                        EventBusUtil.sendEvent(new Event(EC.EventCode.HINT_MINE_RED_DOT));//在我的界面中隐藏红点
+                                    }
+                                }
+                            });
 
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
             }
+
         }
     };
 
