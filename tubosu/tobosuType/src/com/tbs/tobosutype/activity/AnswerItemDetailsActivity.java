58190f0718d1efddacc4ace2718ff6f008106345
Lin <lin591025@163.com>
2018-11-06 11:43:31 +0800

    增加装修问答详情页

---
+++ tubosu/tobosuType/src/com/tbs/tobosutype/activity/AnswerItemDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.tbs.tobosutype.activity;
+
+import android.os.Bundle;
+
+import com.tbs.tobosutype.R;
+import com.tbs.tobosutype.base.BaseActivity;
+
+/**
+ * Created by Mr.Wang on 2018/11/6 11:39.
+ */
+public class AnswerItemDetailsActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_answer_details);
+    }
+}
